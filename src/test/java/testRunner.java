70dffe5c7d2b62b63467044f8bacf5b1bb90a8bb
Niraj Kumar <niraj098@gmail.com>
2022-04-15 00:42:03 +0530

    Updated Script

--- src/test/java/testRunner.java
+++ src/test/java/testRunner.java
@@ -1,12 +1,19 @@
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue ={"stepDefs"},
-        format = {"pretty","html:target/cucumber"}
+        features = {
+                "src/test/resources/features"
+        },
+        plugin = {
+                "pretty",
+                "html:results/html",
+                "json:results/json/result.json",
+                "junit:results/junit/cucumber.xml"
+        },
+        monochrome = true
 )
 
 public class testRunner {
