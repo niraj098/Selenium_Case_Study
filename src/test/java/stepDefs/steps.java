9d7b3b84e1f792b327141ae5de69c7046f4c1285
Niraj Kumar <niraj098@gmail.com>
2022-04-15 21:56:22 +0530

    Updated Script

--- src/test/java/stepDefs/steps.java
+++ src/test/java/stepDefs/steps.java
@@ -1,14 +1,22 @@
 package stepDefs;
 
 import io.cucumber.java.en.Given;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class steps {
     WebDriver driver =null;
 
-    @Given("^I have open the browser$")
+    @Given("I have open the browser")
+    public void launchQAScript() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+
+    }
+
+
+    /*@Given("^I have open the browser$")
     public void openBrowser() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","/Users/nirajkumar/Downloads/chromedriver");
         //WebDriverManager.chromedriver().setup();
@@ -17,6 +25,6 @@ public class steps {
         driver.manage().window().maximize();
         driver.get("https://www.amazon.com");
         Thread.sleep(5000);
-    }
+    }*/
 }
 ///Users/nirajkumar/Downloads/chromedriver.exe
